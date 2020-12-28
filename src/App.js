7ea8db917980c21fd2408319afdf777cc3ee446e
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-28 14:31:27 -0800

    addedd svg pattern background. took  my while to figure out the positioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
+import { Navbar } from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
+	const [ menu, setMenu ] = useState(false);
+
 	return (
 		<div>
-			<h1>Modern Landing Page</h1>
+			<Navbar active={menu} setActive={setMenu} />
 		</div>
 	);
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import Logo from '../../images/logo.svg';
+import { HamburgerSpin } from 'react-animated-burgers';
+import PatternBg from '../../images/bg-tablet-pattern.svg';
+import './Navbar.scss';
+export const Navbar = ({ active, setActive }) => {
+	const toggle = () => {
+		setActive((prevState) => !prevState);
+	};
 
-export const Navbar = () => {
-	return <nav />;
+	return (
+		<nav>
+			<div className='inner-nav-container'>
+				<div className='logo-container'>
+					<img src={Logo} alt='company-logo' />
+				</div>
+
+				<HamburgerSpin
+					isActive={active}
+					toggleButton={toggle}
+					barColor='#242D52'
+					buttonWidth={30}
+					buttonStyle={{ padding: 0 }}
+				/>
+			</div>
+			<div className='pattern-bg'>
+				<img src={PatternBg} alt='bg-icon' />
+			</div>
+		</nav>
+	);
 };
