9940e927ebbd799da063efd7e510a310a56cf2d7
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-29 11:39:15 -0800

    hero-section almost complete. only thing left is to add the primary button

--- src/components/HeroSection/Herosection.js
+++ src/components/HeroSection/Herosection.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { SectionLayout } from '../SectionLayout/SectionLayout';
 import metric from '../../images/illustration-intro.svg';
+import './HeroSection.scss';
+
 export const Herosection = () => {
 	const HeroText = () => {
 		return (
-			<h1>Bring everyone together to build better products </h1>,
-			(
+			<React.Fragment>
+				<h1>Bring everyone together to build better products.</h1>
 				<span>
-					Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view
+					Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in
+					view.
 				</span>
-			)
+			</React.Fragment>
 		);
 	};
 
@@ -17,5 +20,13 @@ export const Herosection = () => {
 		return <img src={metric} alt='hero-img' />;
 	};
 
-	return <SectionLayout contentOne={<MetricGraphs />} contentTwo={<HeroText />} />;
+	return (
+		<SectionLayout
+			contentOne={<MetricGraphs />}
+			contentTwo={<HeroText />}
+			sectionClass={'hero-section'}
+			contentOneClass={'metric-img-container'}
+			contentTwoClass={'text-container'}
+		/>
+	);
 };
