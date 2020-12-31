3c2c5a4420ad1abc8f3d1ef0c8a97d39cc78a3db
Gabriel Anguiano <anguianog97@gmail.com>
2020-12-31 15:55:08 -0800

    footer section complete. pretty much project is done. only thing left is adjustresponsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,31 @@ import React from 'react';
 import './Footer.scss';
 import { PrimaryButton } from '../Button/PrimaryButton';
 import { ReactComponent as Logo } from '../../images/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faYoutube, faPinterest, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+const socialLinks = [
+	{
+		name: faFacebookSquare,
+		site: 'https://facebook.com'
+	},
+	{
+		name: faYoutube,
+		site: 'https://youtube.com'
+	},
+	{
+		name: faTwitter,
+		site: 'https://twitter.com'
+	},
+	{
+		name: faPinterest,
+		site: 'https://pinterest.com'
+	},
+	{
+		name: faInstagram,
+		site: 'https://instagram.com'
+	}
+];
 export const Footer = () => {
 	return (
 		<div className='footer-section'>
@@ -42,8 +67,19 @@ export const Footer = () => {
 				</div>
 			</div>
 			<div className='social-links-logo-container'>
-				<div className='social-links' />
+				<div className='social-links'>
+					{socialLinks.map((link) => {
+						return (
+							<a href={link.site}>
+								<FontAwesomeIcon icon={link.name} size='2x' />
+							</a>
+						);
+					})}
+				</div>
 				<Logo className='logo' />
+			</div>
+			<div className='copyright-container'>
+				<p>Copyright 2020.All Rights Reserved</p>
 			</div>
 		</div>
 	);
