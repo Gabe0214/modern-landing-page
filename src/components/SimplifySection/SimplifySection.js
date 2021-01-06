d9341c090057bf63b0d2197bd3831db2b1e53af3
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-05 16:55:18 -0800

    footer section responsiveness complete for screen sizes 1024 thorough 1280. resoponsiveness styling can be quite challenging :/

--- src/components/SimplifySection/SimplifySection.js
+++ src/components/SimplifySection/SimplifySection.js
@@ -5,9 +5,7 @@ import mobileSimplifyPatter from '../../images/bg-simplify-section-mobile.svg';
 export const SimplifySection = () => {
 	return (
 		<div className='simplify-section'>
-			<p>
-				Simplify how <br />your team works today.
-			</p>
+			<p>Simplify how your team works today.</p>
 			<PrimaryButton placeholder={'Get Started'} />
 			<img src={mobileSimplifyPatter} alt='mobile-pattern' />
 		</div>
